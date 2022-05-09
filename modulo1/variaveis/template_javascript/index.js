e7ecb41cc418b0725d18d822c3fb6de1d91776f5
Insider <jvberga@gmail.com>
2022-05-09 20:09:58 -0300

    exercicios-de-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,61 @@
+// Exercícios de interpretação de código
+
+/* let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b) */
+
+// Será impresso no console os valores de 'a' e 'b'
+
+/* let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c) */
+
+// Será impresso no console os valores de 'a', 'b' e 'c', porém, em 'c' repetirá o valor de 'a'
+
+/* let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+// Esses comandos servem para pegar informações do usuário que está acessando o site
+// Sugiro let horasTrabalhadas pra primeira e let valorDiário pra segunda
+
+// ============================================================================================
+
+// Exercícios de escrita de código
+
+/* const nomeUsuário = undefined
+const idadeUsuário = undefined
+console.log(typeof nomeUsuário, idadeUsuário) */
+
+// Os tipos foram impressos de forma indefinida para que possam ser preenchidos posteriormente
+
+/* const seuNome = prompt("Qual o seu nome?")
+const suaIdade = prompt("Qual a sua idade?")
+console.log(typeof seuNome,suaIdade)
+console.log(seuNome, suaIdade) */
+
+/* let palmeiras = undefined
+let comeFrutas = undefined
+let seHidratou = undefined
+palmeiras = true
+comeFrutas = true
+seHidratou = true */
+
+/* palmeiras = prompt ("Você é Palmeirense?")
+comeFrutas = prompt ("Você come frutas?")
+seHidratou = prompt ("Você se hidratou?") */
+
+let a = 25
+let b = 10
+let c = 10
+c = b
+b = a
+b = c
+console.log (a, b, c,)
