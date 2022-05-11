8056556c97726d9d8370923d61c3cafb10321820
Insider <jvberga@gmail.com>
2022-05-11 00:27:45 -0300

    operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,70 @@
+// Exercícios de interpretação de código
+// 1.
+
+/* const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado) */
+
+// 'a' false, 'b' false, 'c' e 'd' bolean
+
+// 2.
+
+/* let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) */
+
+// nesse código não haverá soma, será impresso no console os números juntos
+
+// 3.
+
+/* let primeiroNumero = prompt("Digite um número!")
+let = segundoNumero = prompt("Digite outro número!")
+
+const soma= primeiroNumero + segundoNumero
+    const res = Number(primeiroNumero) + Number(segundoNumero)
+    alert("Reultado da Operação "+ res) */
+
+// aqui está a solução para o meu "colega" ter o valor impresso no console
+
+// Exercícios de escrita de código
+// 1.
+
+/* let idadeUser = prompt("Qual a sua idade?")
+let idadeBest = prompt("Qual a idade do seu melhor amigo (a) ?")
+const resposta = true
+const subtrair = idadeUser - idadeBest
+console.log("Sua idade é maior do que a do seu melhor amigo (a) ?", true, subtrair, "anos mais velho") */
+
+// 2.
+
+/* let numeroPar = prompt("Insira um número par!")
+const restoDiv = 15 % 2
+console.log(restoDiv) */
+
+// Notei que números pares sempre dão 0, números ímpares sempre dão 1
+
+// 3.
+
+/* let idadeUser = Number (prompt ("Qual sua idade em anos?"))
+const idadeMeses = 300
+const idadeDias = 9125
+const idadeHoras = 219000
+console.log("Sua idade em meses", idadeMeses, "Sua idade em dias", idadeDias, "Sua idade em horas", idadeHoras) */
+
+// 4.
+
+// Não consegui realizar esse exercício a tempo, pois tive muita dificuldade com os outros :(
